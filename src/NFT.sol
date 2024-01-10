6fb217c8ae7d97857a18a0e395cbd13315574379
samigabor <samoila.gabor@yahoo.com>
2024-01-10 18:25:10 +0200

    feat: add NFT contract and remapping

---
+++ src/NFT.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFT is ERC721 {
+    uint256 public s_tokenCounter;
+
+    mapping(uint256 => string) public s_tokenIdToUri;
+
+    constructor() ERC721("Doggie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function safeMint(string memory tokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return s_tokenIdToUri[tokenId];
+    }
+}
