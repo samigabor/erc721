fee65ee8de30d49a39e41dc5e0a8a0896a129106
samigabor <samoila.gabor@yahoo.com>
2024-01-10 23:02:36 +0200

    feat: deploy and interact on Sepolia

--- script/DeployNft.s.sol
+++ script/DeployNft.s.sol
@@ -12,6 +12,12 @@ import {NFT} from "../src/NFT.sol";
  --broadcast \
  --private-key=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
  @dev use --interactive for private key with real money
+
+ forge script script/DeployNft.s.sol \
+ --rpc-url $RPC_URL \
+ --broadcast \
+ --private-key=$PRIVATE_KEY \
+ --verify --etherscan-api-key $ETHERSCAN_API_KEY
 */
 contract DeployNft is Script {
     function run() external returns (NFT nft) {
--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -13,6 +13,11 @@ import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
  --broadcast \
  --private-key=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
  @dev use --interactive for private key with real money
+
+ forge script script/Interactions.s.sol \
+ --rpc-url $RPC_URL \
+ --broadcast \
+ --private-key=$PRIVATE_KEY
 */
 contract MintNft is Script {
     string public constant uri = "ipfs://Qmc9JxfqaccjBxjY44aCugErfRDvmCzF8JvpnDibMgoses";
