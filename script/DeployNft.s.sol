b33ea914e2c2f0023070711dcdc865110577c238
samigabor <samoila.gabor@yahoo.com>
2024-01-10 18:54:29 +0200

    feat: add deploy script

---
+++ script/DeployNft.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License_Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {Script} from "forge-std/Script.sol";
+import {NFT} from "../src/NFT.sol";
+
+/** 
+ Deploy to Anvil (anvil chain started in a different terminal): 
+ forge script script/DeployNft.s.sol \
+ --rpc-url http://127.0.0.1:8545 \
+ --broadcast \
+ --private-key=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+ @dev use --interactive for private key with real money
+*/
+contract DeployNft is Script {
+    function run() external returns (NFT nft) {
+        vm.startBroadcast();
+        nft = new NFT();
+        vm.stopBroadcast();
+    }
+}
